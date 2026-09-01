ea51ee7a1a7ee0cf414c20b486c16025c4594b27
Jorge Arenas <jorgearenass97@gmail.com>
2026-09-01 11:50:40 +0200

    Redirect & Forward

--- src/main/java/com/jorge/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/jorge/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
